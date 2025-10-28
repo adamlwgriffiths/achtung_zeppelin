8c59eb182f5915513e9b1ed72eb850627cde4119
Adam Griffiths <adam.lw.griffiths@gmail.com>
2025-10-28 22:06:14 +1100

    Add project files

---
+++ resource.h
@@ -0,0 +1,69 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WinMain.rc
+//
+#define IDI_MAIN_ICON                   101
+#define IDR_MAIN_ACCEL                  113
+#define IDR_MENU                        141
+#define IDD_SELECTDEVICE                144
+#define IDD_DIALOG1                     146
+#define IDD_SETTINGS_WORLDCREATION      146
+#define IDD_HELP_ABOUT                  148
+#define IDD_HELP_HELP                   149
+#define IDC_DEVICE_COMBO                1000
+#define IDC_ADAPTER_COMBO               1002
+#define IDC_ADAPTERFORMAT_COMBO         1003
+#define IDC_RESOLUTION_COMBO            1004
+#define IDC_MULTISAMPLE_COMBO           1005
+#define IDC_REFRESHRATE_COMBO           1006
+#define IDC_BACKBUFFERFORMAT_COMBO      1007
+#define IDC_DEPTHSTENCILBUFFERFORMAT_COMBO 1008
+#define IDC_VERTEXPROCESSING_COMBO      1009
+#define IDC_PRESENTINTERVAL_COMBO       1010
+#define IDC_MULTISAMPLE_QUALITY_COMBO   1011
+#define IDC_WINDOW                      1016
+#define IDC_FULLSCREEN                  1018
+#define IDC_HEIGHT_SCALE                1031
+#define IDC_FRACTAL_AMPLITUDE           1032
+#define IDC_FLAT_LENGTH                 1033
+#define IDC_FLAT_WIDTH                  1034
+#define IDC_GAME_NUMCITIES              1035
+#define IDC_FRACTAL_ROUGHNESS           1036
+#define IDC_RADIO_TERRAIN_FLAT          1037
+#define IDC_RADIO_TERRAIN_SPHERE        1038
+#define IDC_RADIO_DIFFICULTY_EASY       1039
+#define IDC_RADIO_DIFFICULTY_MEDIUM     1040
+#define IDC_RADIO_DIFFICULTY_HARD       1041
+#define IDC_GAME_NUMSHIPS               1042
+#define IDC_SPHERICAL_RADIUS            1043
+#define IDC_FLAT_SPACING                1044
+#define IDC_TEXTURE_SIZE                1045
+#define IDC_SPHERICAL_STACKS            1046
+#define IDC_SPHERICAL_SLICES            1047
+#define IDC_CHECK1                      1048
+#define IDC_DEMO_MODE                   1048
+#define IDM_TOGGLEHELP                  40001
+#define IDM_CHANGEDEVICE                40002
+#define IDM_TOGGLEFULLSCREEN            40003
+#define IDM_TOGGLESTART                 40004
+#define IDM_SINGLESTEP                  40005
+#define IDM_EXIT                        40006
+#define ID_VIEW_NEXTSHAPE               40011
+#define ID_SETTINGS_WORLDCREATION       40012
+#define ID_FILE_RESTART                 40014
+#define ID_HELP_ABOUT                   40016
+#define ID_FILE_NEW                     40018
+#define ID_GAME_CHANGEVIEW              40020
+#define ID_HELP_HELP                    40022
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        150
+#define _APS_NEXT_COMMAND_VALUE         40023
+#define _APS_NEXT_CONTROL_VALUE         1049
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
